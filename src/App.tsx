10583aa7f45a8f5ce195012eea51e86814646e84
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-03-08 14:47:02 +0200

    add hello world text

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 
 export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          Create develop branch
-        </a>
-        <h1>Test commit</h1>
-      </header>
-    </div>
+    <h1>Hello, world!</h1>
   );
 }
